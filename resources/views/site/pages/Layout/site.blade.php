df0a7b3cd08bbe470a510a2c7e95d8cdbc691a93
Ayax Córdova <ayax.cordova@aydev.mx>
2024-07-16 13:07:40 -0600

    Style address bar and scroll bar

--- resources/views/site/pages/Layout/site.blade.php
+++ resources/views/site/pages/Layout/site.blade.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="theme-color" content="#33ff33">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <title>Home</title>
