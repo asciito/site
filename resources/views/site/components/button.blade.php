db3283006d99de7ad7f690da7df9e28c93e0d517
Ayax Córdova <ayax.cordova@aydev.mx>
2024-07-16 19:00:46 -0600

    Fix button state for button and anchor

--- resources/views/site/components/button.blade.php
+++ resources/views/site/components/button.blade.php
@@ -15,21 +15,39 @@
 
     $attributes = $attributes->class([
         $sizeClasses,
+        'select-none',
         'text-slate-800',
         'flex justify-between items-center',
-        'px-6 py-2 bg-harlequin enabled:hover:bg-harlequin-600 enabled:active:bg-harlequin-400 cursor-pointer',
+        'px-6 py-2 bg-harlequin',
+        'cursor-pointer',
         'w-full' => $shouldExpand,
     ]);
+
 @endphp
 
 <div @class(['w-full flex flex-wrap', $positionClass])>
-    @empty($href)
-        <button {{ $attributes }}>
-            {{ $slot }}
-        </button>
-    @else
-        <a href="{{ $href }}" {{ $attributes }}>
+    @isset($href)
+        <a
+            href="{{ $href }}"
+            {{ $attributes->class([
+                'enabled',
+                '[&.disabled]:opacity-80 [&.disabled]:cursor-not-allowed',
+                '[&.enabled]:hover:bg-harlequin-600 [&.enabled]:active:bg-harlequin-400',
+            ]) }}
+            wire:loading.class="disabled"
+            wire:loading.class.remove="enabled"
+        >
             {{ $slot }}
         </a>
+    @else
+        <button
+            {{ $attributes->class([
+                'disabled:opacity-80 disabled:cursor-not-allowed',
+                'enabled:hover:bg-harlequin-600 enabled:active:bg-harlequin-400',
+            ]) }}
+            wire:loading.attr="disabled"
+        >
+            {{ $slot }}
+        </button>
     @endisset
 </div>
