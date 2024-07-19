7affe7ca8933b243a9877ff0c9289be41e6490ff
Ayax Córdova <ayax.cordova@aydev.mx>
2024-07-18 22:56:01 -0600

    Add post date information

--- resources/views/site/pages/post.blade.php
+++ resources/views/site/pages/post.blade.php
@@ -1,5 +1,13 @@
 <x-site::layout>
-    <h1 class="text-4xl mb-8 text-slate-800">{{ \Illuminate\Support\Str::title($post->title) }}</h1>
+    <div class="mb-8 space-y-2">
+        <h1 class="text-4xl text-slate-800">{{ \Illuminate\Support\Str::title($post->title) }}</h1>
+
+        <p class="text-blue-600 text-sm">
+            <span>{{ $post->created_at->equalTo($post->updated_at) ? 'Created on' : 'Last updated on' }}</span>
+            <time>{{ $post->updated_at->format('F d, Y') }}</time>
+        </p>
+    </div>
+
 
     <img src="" alt="">
 
