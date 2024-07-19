7caeaf179261c4e4d05eaccd5a32d9ac540530df
Ayax Córdova <ayax.cordova@aydev.mx>
2024-07-18 21:28:59 -0600

    Remove unnecessary group

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,9 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::group(['middleware' => 'guest'], function () {
-    Route::get('/', [SiteController::class, 'index'])->name('home');
-    Volt::route('contact/', 'contact')->name('contact');
+Route::get('/', [SiteController::class, 'index'])->name('home');
+Volt::route('contact/', 'contact')->name('contact');
 
-    Route::get('/{post:slug}', function (Post $post) {
-       return  view('site::pages.post', ['post' => $post]);
-    })->name('post');
-});
+Route::get('/{post:slug}', function (Post $post) {
+   return  view('site::pages.post', ['post' => $post]);
+})->name('post');
