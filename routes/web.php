0928c3ae342e937e39d68d242a9f6021ba71a451
Ayax Córdova <ayax.cordova@aydev.mx>
2024-08-10 21:39:23 -0600

    Change uri

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(\App\Http\Middleware\CustomTrackPageview::class)->group(function () {
     Route::get('/', [SiteController::class, 'index'])->name('home');
-    Route::get('contact/', [SiteController::class, 'contact'])->name('contact');
+    Route::get('/contact', [SiteController::class, 'contact'])->name('contact');
     Route::get('/terms-of-use', [SiteController::class, 'terms'])->name('terms');
     Route::get('/privacy-policy', [SiteController::class, 'privacy'])->name('privacy');
 
