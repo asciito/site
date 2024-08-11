0681661dcd404d355089307e2120d797046df0ea
Ayax Córdova <ayax.cordova@aydev.mx>
2024-08-10 23:02:52 -0600

    Add alias for middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->prepend(\Torchlight\Middleware\RenderTorchlight::class);
+        $middleware
+            ->prepend(\Torchlight\Middleware\RenderTorchlight::class)
+            ->alias([
+                'track' => \App\Http\Middleware\CustomTrackPageView::class,
+            ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
