a9dfa02953ee75dcf98bebf0ef66cd8da5f656eb
asciito <asciito@users.noreply.github.com>
2024-08-11 03:40:27 +0000

    fix code style

--- app/Http/Middleware/CustomTrackPageView.php
+++ app/Http/Middleware/CustomTrackPageView.php
@@ -3,11 +3,9 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Pirsch\Http\Middleware\TrackPageview;
-use Pirsch\Pirsch;
 use Symfony\Component\HttpFoundation\Response;
 
 class CustomTrackPageView extends TrackPageview
