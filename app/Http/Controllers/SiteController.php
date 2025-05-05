c418a05b149ade012d87c7b0890e2cf2f9e45d60
Ayax Córdova <ayax.cordova@aydev.mx>
2025-05-05 17:33:10 -0600

    [FEAT]: Add user description
    * Add description field
    
    * Update user profile display

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
@@ -9,7 +10,9 @@ class SiteController extends Controller
 {
     public function index(): Request|View
     {
-        return view('site::pages.home');
+        return view('site::pages.home', [
+            'user' => User::first(),
+        ]);
     }
 
     public function contact(): Request|View
--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements FilamentUser, HasAvatar
         'name',
         'email',
         'password',
+        'description',
     ];
 
     /**
