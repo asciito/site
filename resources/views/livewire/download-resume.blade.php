fdfe61a2484ece3f447e6bacf54fb29d8ca691a2
asciito <ayax.cordova@aydev.mx>
2025-05-06 20:09:36 -0600

    [FEAT]: Add resume upload in profile page

--- resources/views/livewire/download-resume.blade.php
+++ resources/views/livewire/download-resume.blade.php
@@ -3,12 +3,24 @@
 use Livewire\Volt\Component;
 
 new class extends Component {
-    public function download(): \Symfony\Component\HttpFoundation\BinaryFileResponse
+    public function resumeExists(): bool
     {
-        return response()->download(
-            public_path('ayax_cordova_resume.pdf')
-        );
+        return \Illuminate\Support\Facades\Storage::exists('resume.pdf');
+    }
+
+    public function download(): ?\Symfony\Component\HttpFoundation\StreamedResponse
+    {
+        if (!$this->resumeExists()) {
+            return null;
+        }
+
+        return \Illuminate\Support\Facades\Storage::download('resume.pdf');
     }
 }; ?>
 
-<x-site::button wire:click="download" should-expand>DOWNLOAD RESUME</x-site::button>
+<x-site::button
+    wire:click="download"
+    wire:confirm="Are you ok with downloading my resume?"
+    should-expand>
+        {{ $this->resumeExists() ? 'Download my resume' : 'Resume not available' }}
+</x-site::button>
