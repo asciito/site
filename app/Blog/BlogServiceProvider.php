096b79ff72ebcf14f682c33b83da6ffbf7e962a2
asciito <ayax.cordova@aydev.mx>
2025-05-10 16:17:29 -0600

    Refactor BlogServiceProvider to mount Livewire components

--- app/Blog/BlogServiceProvider.php
+++ app/Blog/BlogServiceProvider.php
@@ -5,11 +5,19 @@ declare(strict_types=1);
 namespace App\Blog;
 
 use Illuminate\Support\ServiceProvider;
+use Livewire\Volt\Volt;
 
 class BlogServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->loadViewsFrom(resource_path('views/blog'), 'blog');
+    }
+
+    public function boot(): void
+    {
+        Volt::mount([
+            resource_path('views/blog/livewire')
+        ]);
     }
 }
