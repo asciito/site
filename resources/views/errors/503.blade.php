47a3dd9a326813089e23e503a6a3072ba187d76e
Ayax Córdova <ayax.cordova@aydev.mx>
2025-05-10 18:53:37 -0600

    [FEAT]: add maintenance mode description in error template

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,5 +1,10 @@
 <x-site::error-layout :code="503" :title="__('Service Unavailable')">
     <x-slot:message>
-        {{ __('Service Unavailable') }}
+        @if(! app()->maintenanceMode())
+            <p>{{ __('Service Unavailable') }}<p>
+        @else
+            <p>{{ __('The application is in maintenance mode.') }}</p>
+            <p>{{ __('Please check back later.') }}</p>
+        @endif
     </x-slot:message>
 </x-site::error-layout>
