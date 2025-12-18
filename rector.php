716c68afa4b3fbde111ec485529dc229ff2a78c6
asciito <ayax.cordova@aydev.mx>
2025-12-18 13:01:31 -0600

    Add rector for code refactoring and add scripts to composer

---
+++ rector.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/app',
+        __DIR__.'/bootstrap',
+        __DIR__.'/config',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
+        __DIR__.'/tests',
+    ])
+    ->withPhpSets()
+    ->withRules([
+        TypedPropertyFromStrictConstructorRector::class,
+    ])
+    ->withImportNames()
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true
+    );
