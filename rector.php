bf0bc77b1f7eee95e8381bf3691dda1bfeccda61
asciito <ayax.cordova@aydev.mx>
2025-12-18 17:04:17 -0600

    Remove `bootstrap` folder from refactoring

--- rector.php
+++ rector.php
@@ -8,7 +8,6 @@ use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRec
 return RectorConfig::configure()
     ->withPaths([
         __DIR__.'/app',
-        __DIR__.'/bootstrap',
         __DIR__.'/config',
         __DIR__.'/resources',
         __DIR__.'/routes',
