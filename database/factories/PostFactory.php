05cd8b957d1d2541df81a777306db4e65024fadf
asciito <ayax.cordova@aydev.mx>
2025-12-19 01:29:00 -0600

    Simplify the HTML generation
    The past implementation adds not only HTML difficult to parse, also adds a lot of garbage like forms, even when the `Rich Editor` ignore that, the renderer takes a lot of time, so I decided to generate my own "random" HTML generator, and with this, I extract a way to also use the same approach for Markdown.

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -13,6 +13,22 @@ class PostFactory extends Factory
 {
     protected $model = Post::class;
 
+    protected array $tags = [
+        'p' => null,
+        'ul' => null,
+        'ol' => null,
+        'li' => ['-', '+', '*'],
+        'strong' => '**',
+        'i' => '*',
+        'h1' => '#',
+        'h2' => '##',
+        'h3' => '###',
+        'h4' => '####',
+        'h5' => '#####',
+        'h6' => '######',
+        'blockquote' => '>',
+    ];
+
     /**
      * Define the model's default state.
      *
@@ -28,41 +44,84 @@ class PostFactory extends Factory
         ];
     }
 
-    protected function fakeHtml(): string
+    public function fakeHtml(): string
     {
-        $content = fake()->randomHtml(10, 10);
-
-        return (string) Str::of($content)
-            ->match('~<body>(.*?)</body>~s')
-            ->replaceMatches('~(<h1>.*?</h1>)~', '');
-    }
-
-    protected function fakeMarkdown(): string
-    {
-        $content = '';
-
-        $content .= '## '.fake()->sentence(random_int(5, 8));
+        $content = $this->createElement(fake()->sentence(random_int(5, 8)), 'h2');
 
         foreach (range(1, random_int(2, 10)) as $_) {
             $paragraphs = fake()->paragraphs(random_int(1, 5));
 
             foreach ($paragraphs as $paragraph) {
-                $content .= "\n\n".$paragraph;
+                $content .= $this->createElement($paragraph, 'p');
             }
 
             if (fake()->boolean()) {
+                $list = '';
 
                 foreach (range(1, random_int(1, 7)) as $_) {
-                    $content .= "\n\n".fake()->randomElement(['*', '-', '+']).' '.fake()->sentence(random_int(5, 8));
+                    $list .= $this->createElement(fake()->sentence(random_int(5, 8)), 'li');
                 }
+
+                $content .= $this->createElement($list, fake()->randomElement(['ul', 'ol']));
             }
 
-            $content .= "\n\n".fake()->randomElement(['###', '####']).' '.fake()->sentence(random_int(5, 8));
+            if (fake()->boolean(random_int(10, 25))) {
+                $content .= $this->createElement(fake()->paragraph(4), 'blockquote');
+            }
 
-            $content .= "\n\n".fake()->paragraph(1);
+            $heading = fake()->randomElement(['h2', 'h3', 'h4', 'h5', 'h6']);
+            $content .= $this->createElement(fake()->sentence(random_int(5, 8)), $heading);
+
+            $content .= $this->createElement(fake()->paragraph(1), 'p');
         }
 
         return $content;
+    }
+
+    public function fakeMarkdown(): string
+    {
+        $newLines = "\n\n";
+
+        $content = $this->createElement(fake()->sentence(random_int(5, 8)), 'h2', true).$newLines;
+
+        foreach (range(1, random_int(2, 10)) as $_) {
+            $paragraphs = fake()->paragraphs(random_int(1, 5));
+
+            foreach ($paragraphs as $paragraph) {
+                $content .= $this->createElement($paragraph, 'p', true).$newLines;
+            }
+
+            if (fake()->boolean()) {
+                $list = '';
+
+                foreach (range(1, random_int(1, 7)) as $_) {
+                    $list .= $this->createElement(fake()->sentence(random_int(5, 8)), 'li', true)."\n";
+                }
+
+                $content .= $list ? "$list\n" : '';
+            }
+
+            if (fake()->boolean(random_int(10, 25))) {
+                $content .= $this->createElement(fake()->paragraph(4), 'blockquote', true).$newLines;
+            }
+
+            $heading = fake()->randomElement(['h2', 'h3', 'h4']);
+            $content .= $this->createElement(fake()->sentence(random_int(5, 8)), $heading, true).$newLines;
+
+            $content .= $this->createElement(fake()->paragraph(1), 'p', true).$newLines;
+        }
+
+        return $content;
+    }
+
+    public function withHtmlContent(): static
+    {
+        return $this->state(fn () => ['content' => $this->fakeHtml()]);
+    }
+
+    public function withMarkdownContent(): static
+    {
+        return $this->state(fn () => ['content' => $this->fakeMarkdown()]);
     }
 
     public function published(): static
@@ -80,5 +139,24 @@ class PostFactory extends Factory
     public function dontSyncSlug(): static
     {
         return $this->state(fn (array $attributes) => ['slug' => Str::slug(fake()->unique()->sentence())]);
+    }
+
+    protected function createElement(string $text, string $tag, bool $asMarkdown = false): string
+    {
+        if (! array_key_exists($tag, $this->tags)) {
+            throw new \InvalidArgumentException("Tag '$tag' is not available.");
+        }
+
+        if ($asMarkdown) {
+            $tag = $this->tags[$tag];
+
+            if ($tag === null) {
+                return $text;
+            }
+
+            $tag = is_array($tag) ? fake()->randomElement($tag) : $tag;
+        }
+
+        return $asMarkdown ? "$tag $text" : "<$tag>$text</$tag>";
     }
 }
--- resources/views/blog/filament/forms/fields/preview.blade.php
+++ resources/views/blog/filament/forms/fields/preview.blade.php
@@ -6,6 +6,6 @@
     <div class="fi-prose">
         <h1>{{ $record->title }}</h1>
 
-        {{ $record->getContent(withTorchlight: false) }}
+        {{ $record->getContent(withTorchlight: true) }}
     </div>
 </div>
