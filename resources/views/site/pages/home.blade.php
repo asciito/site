20c319cf8cc29970cae061e8702e84ba863d45e2
Ayax Córdova <ayax.cordova@aydev.mx>
2025-12-20 17:24:05 -0600

    style: normalize dynamic content style and update style for separator

--- resources/views/site/pages/home.blade.php
+++ resources/views/site/pages/home.blade.php
@@ -11,7 +11,7 @@
 
 <x-site::layout :page="$seoData">
     <header class="block text-center space-y-4 mb-8">
-        <div class="prose-lg prose-a:active:text-harlequin-600 prose-a:hover:text-harlequin-600 prose-a:transition-colors prose-a:duration-75">
+        <div class="content">
             @if ($introduction = $user?->introduction)
                 {!! str($introduction)->markdown()->toHtmlString() !!}
             @else
@@ -35,7 +35,7 @@
 
         <h3 class="text-3xl mb-8 text-center">{{ $user?->name ?? 'User not created' }}</h3>
 
-        <div class="prose prose-pre:ring-1 prose-pre:ring-black/5 prose-pre:shadow-sm max-w-none mt-8  prose-a:active:text-harlequin-600 prose-a:hover:text-harlequin-600 prose-a:transition-colors prose-a:duration-75">
+        <div class="content">
             @if ($description = $user?->description)
                 {!! str($description)->markdown()->sanitizeHtml() !!}
             @else
