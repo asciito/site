3adbb4d42585fff9fec2e7edb919da41218a05d1
asciito <ayax.cordova@aydev.mx>
2025-12-22 13:28:05 -0600

    Add fields to upload images
    Two form fields were added to handle the SEO image to be used for all the pages if no image is provided per page, and a field for the favicon

--- app/Filament/Pages/SiteSettings.php
+++ app/Filament/Pages/SiteSettings.php
@@ -4,10 +4,14 @@ namespace App\Filament\Pages;
 
 use App\Filament\Pages\SettingsPage as Page;
 use App\Settings\SiteSettings as Settings;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Group;
 use Filament\Schemas\Components\Section;
+use Filament\Support\Colors\Color;
+use Filament\Support\Icons\Heroicon;
+use Illuminate\Validation\Rule;
 
 class SiteSettings extends Page
 {
@@ -27,29 +31,90 @@ class SiteSettings extends Page
                     Textarea::make('description')
                         ->required()
                         ->maxLength(120),
-                    // TODO: Add logo, media and favicon uploaders
                     Group::make([
-                        TextInput::make('twitter_handler')
-                            ->label('X/Twitter')
-                            ->required()
-                            ->prefixIcon('fab-x-twitter'),
-                        TextInput::make('facebook_handler')
-                            ->label('Facebook')
-                            ->required()
-                            ->prefixIcon('fab-facebook-f'),
-                        TextInput::make('instagram_handler')
-                            ->label('Instagram')
-                            ->required()
-                            ->prefixIcon('fab-instagram'),
-                        TextInput::make('linkedin_handler')
-                            ->label('LinkedIn')
-                            ->required()
-                            ->prefixIcon('fab-linkedin'),
-                        TextInput::make('github_handler')
-                            ->label('Github')
-                            ->required()
-                            ->prefixIcon('fab-github'),
+                        FileUpload::make('image')
+                            ->label(__('SEO image'))
+                            ->validationAttribute('image')
+                            ->image()
+                            ->imageEditor()
+                            ->imageEditorAspectRatios([
+                                null,
+                                '1.91:1',
+                            ])
+                            ->imageResizeTargetWidth('1200')
+                            ->imageResizeTargetHeight('630')
+                            ->imageCropAspectRatio('1.91:1')
+                            ->panelLayout(null)
+                            ->previewable(false)
+                            ->rules([
+                                Rule::dimensions()
+                                    ->ratio(1.91)
+                                    ->minWidth(1200)
+                                    ->maxWidth(1920),
+                            ])
+                            ->hintIcon(Heroicon::InformationCircle)
+                            ->hintColor(Color::Zinc)
+                            ->hintIconTooltip('Width (min: 1200px - max: 1920px) - Ration (1.91:1)'),
+                        FileUpload::make('favicon')
+                            ->label(__('Favicon'))
+                            ->validationAttribute('favicon')
+                            ->image()
+                            ->imageEditor()
+                            ->imageEditorAspectRatios([
+                                '1:1',
+                            ])
+                            ->imageCropAspectRatio('1:1')
+                            ->panelLayout(null)
+                            ->previewable(false)
+                            ->rules([
+                                Rule::dimensions()
+                                    ->ratio(1)
+                                    ->minWidth(16)
+                                    ->maxWidth(512),
+                            ])
+                            ->hintIcon(Heroicon::InformationCircle)
+                            ->hintColor(Color::Zinc)
+                            ->hintIconTooltip('Size (min: 16px  - max: 512px) - Ration (1:1)'),
+                    ])->columns([
+                        'default' => 1,
+                        'sm' => 1,
+                        'md' => 2,
+                        'lg' => 2,
+                        'xl' => 2,
                     ]),
+                ])
+                ->aside()
+                ->columnSpanFull(),
+            Section::make(__('Social Media'))
+                ->description('Information related to the available social media configuration')
+                ->schema([
+                    Section::make(__('Handlers'))
+                        ->schema([
+                            Group::make([
+                                TextInput::make('twitter_handler')
+                                    ->label('X/Twitter')
+                                    ->required()
+                                    ->prefixIcon('fab-x-twitter'),
+                                TextInput::make('facebook_handler')
+                                    ->label('Facebook')
+                                    ->required()
+                                    ->prefixIcon('fab-facebook-f'),
+                                TextInput::make('instagram_handler')
+                                    ->label('Instagram')
+                                    ->required()
+                                    ->prefixIcon('fab-instagram'),
+                                TextInput::make('linkedin_handler')
+                                    ->label('LinkedIn')
+                                    ->required()
+                                    ->prefixIcon('fab-linkedin'),
+                                TextInput::make('github_handler')
+                                    ->label('Github')
+                                    ->required()
+                                    ->prefixIcon('fab-github'),
+                            ]),
+                        ])
+                        ->collapsed()
+                        ->collapsible(),
                 ])
                 ->aside()
                 ->columnSpanFull(),
