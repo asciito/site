7ea137ebc7afbffb9d4bdf115d961a52e07608df
asciito <ayax.cordova@aydev.mx>
2025-12-25 15:42:16 -0600

    Add `empty` state when `to` date is not given

--- resources/views/site/components/date-range.blade.php
+++ resources/views/site/components/date-range.blade.php
@@ -1,18 +1,28 @@
 @props([
     'from',
     'to' => null,
-    'relative' => false
+    'relative' => false,
+    'emptyStateTo' => null,
 ])
 
 @php
-    use Illuminate\Support\Carbon;use Illuminate\Support\Pluralizer;
+    use Illuminate\Support\Carbon;
+    use Illuminate\Support\HtmlString;
+    use Illuminate\Support\Pluralizer;
+    use Illuminate\View\ViewException;
 
     /**
+     * Both dates will reset it's time to 00:00:00.000000
+     *
      * @var Carbon $from The starting of the date range
      * @var ?Carbon $to The end of the date range if available
      */
+    $from = $from->setTime(0, 0);
+    $to = $to?->setTime(0, 0);
 
-    if ($to && $relative) {
+    throw_if($to?->lessThan($from), ViewException::class, 'The `to` date must be greater than or equal to `from` date');
+
+    if (filled($to) && $relative) {
         $yearsFloat = $from->diffInYears($to); // float
         $years = (int) $yearsFloat;
         $labelYears = $yearsFloat > $years ? $years + 1 : $years;
@@ -27,8 +37,14 @@
     <time datetime="{{ $from->format('Y-m-d') }}">{{ $from->format('M d, Y') }}</time>
     -
     @if ($to)
-        <time datetime="{{ $to->format('Y-m-d') }}">{{ $from->format('M d, Y') }}</time>
+        <time datetime="{{ $to->format('Y-m-d') }}">{{ $to->format('M d, Y') }}</time>
     @else
-        <span>Not available</span>
+        @if ($emptyStateTo instanceof HtmlString)
+            {{ $emptyStateTo }}
+        @elseif(filled($emptyStateTo) && is_string($emptyStateTo))
+            <span>{{ $emptyStateTo }}</span>
+        @else
+            <span>Not available</span>
+        @endif
     @endif
 @endif
