27b647869af72d6da2f68442eaaf166719b35c51
asciito <ayax.cordova@aydev.mx>
2025-12-28 14:23:04 -0600

    Hide scroll button visibility

--- resources/views/site/pages/layout/site.blade.php
+++ resources/views/site/pages/layout/site.blade.php
@@ -98,10 +98,9 @@
                 </div>
             </div>
         </footer>
-    @endif
 
-    <div
-        x-data="{
+        <div
+            x-data="{
             visible: false,
             handleScroll() {
                 const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
@@ -113,16 +112,17 @@
                 }
             }
         }"
-        x-show="visible"
-        x-transition
-        @scroll.window.throttle.25ms="handleScroll"
-        class="fixed bottom-5 right-5 h-10 w-10 drop-shadow-sm">
+            x-show="visible"
+            x-transition
+            @scroll.window.throttle.25ms="handleScroll"
+            class="fixed bottom-5 right-5 h-10 w-10 drop-shadow-sm">
             <x-site::button
                 @click="window.scroll({top: 0, behavior: 'smooth'})"
                 class="relative rounded-full h-10 w-10 p-0!">
                 <x-icon name="heroicon-s-arrow-up" class="w-5 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"/>
             </x-site::button>
-    </div>
+        </div>
+    @endif
 
     @filamentScripts
     @stack('scripts')
