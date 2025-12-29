2d5dcdf923772d0c16199f589705151f4fb4a244
Ayax Córdova <ayax.cordova@aydev.mx>
2025-12-29 13:44:13 -0600

    chore: improve loading speed
    * Remove `axios`dependency and add `alpine-clipboard` dependency
    
    * Reorder CSS input paths in Vite configuration
    
    * Integrate Livewire and Alpine Clipboard for manual building
    
    * Optimize loading by restructuring Livewire and Vite script inclusion

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
                 'resources/css/filament.css',
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
